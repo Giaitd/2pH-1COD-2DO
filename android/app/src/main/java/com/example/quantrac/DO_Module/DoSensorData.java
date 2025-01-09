868bef9bd2d586b4fff34618065e41556bf2ce05
giaikt <giaiemail@gmail.com>
2025-01-09 13:31:43 +0700

    project 2ph-1cod-2do

---
+++ android/app/src/main/java/com/example/quantrac/DO_Module/DoSensorData.java
@@ -0,0 +1,13 @@
+package com.example.quantrac.DO_Module;
+
+public class DoSensorData {
+    public double dO = 0.0D;
+    public double temp = 0.0D;
+
+
+    public DoSensorData(double dO,double temp) {
+        this.dO = dO;
+        this.temp = temp;
+
+    }
+}
